88413c60ba6ce211bdd6257b863f513444bbcae4
nishka322 <nishka322@yandex.ru>
2026-01-04 14:25:00 +0300

    Fix import

--- src/main/java/ru/practicum/shareit/user/repository/DbUserRepository.java
+++ src/main/java/ru/practicum/shareit/user/repository/DbUserRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.exceptions.NotFoundException;
 import ru.practicum.shareit.user.model.User;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
