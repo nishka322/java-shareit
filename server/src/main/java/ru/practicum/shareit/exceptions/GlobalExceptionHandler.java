eef10391e4bd28c3be8a6c711786bcac13403842
nishka322 <nishka322@yandex.ru>
2026-01-12 10:43:12 +0300

    checkstyle

--- server/src/main/java/ru/practicum/shareit/exceptions/GlobalExceptionHandler.java
+++ server/src/main/java/ru/practicum/shareit/exceptions/GlobalExceptionHandler.java
@@ -1,13 +1,10 @@
 package ru.practicum.shareit.exceptions;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
-import java.util.Objects;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
